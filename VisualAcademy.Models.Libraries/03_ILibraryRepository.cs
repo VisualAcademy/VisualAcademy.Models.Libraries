0f7a19902d319aff45669f7a82cb9ade8ba60107
Park YongJune <redplus@redplus.net>
2021-11-24 03:25:41 +0900

    Add project files.

---
+++ VisualAcademy.Models.Libraries/03_ILibraryRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace VisualAcademy.Models.Libraries
+{
+    /// <summary>
+    /// [4] Repository Interface
+    /// </summary>
+    public interface ILibraryRepository : ILibraryCrudRepository<LibraryModel>
+    {
+        Task<Tuple<int, int>> GetStatus(int parentId);
+        Task<bool> DeleteAllByParentId(int parentId);
+        Task<SortedList<int, double>> GetMonthlyCreateCountAsync();
+    }
+}
